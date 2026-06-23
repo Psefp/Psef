45b229bb2fa27e67923a4d3fe8fea0204135383f
Psefp <libaefe@gmail.com>
2026-06-23 21:26:52 +0300

    v1 completed version

--- game/Buildings/Factory.cpp
+++ game/Buildings/Factory.cpp
@@ -1,9 +1,31 @@
 #include "Factory.h"
- Factory::Factory(){
-
-
-
-
-
-    
+ Factory::Factory(int FactoryX ,int FactoryY,int FactoryPopulation){
+ maxpopulation=100;
+ time= 0;
+ storage=0;
+FactoryY=posy;
+FactoryX=posx;
+population=FactoryPopulation;
+proctionrate=10;
  }
+void Factory::calculateStorage()
+    {
+        storage = population / 10;
+        if (population > 100)
+        {
+            storage = storage - (storage * 10);
+        }
+    }
+
+void Factory::calculateTime(){
+     time++ ;
+ calculateStorage();
+
+
+
+};
+
+
+
+
+ 
